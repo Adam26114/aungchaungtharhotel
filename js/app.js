a5b7a0578ec7370bd8c77092fe295693a31d9b34
Adam26114 <zakato26114@gmail.com>
2022-12-05 07:21:48 +0630

    5 Doc 2022

--- js/app.js
+++ js/app.js
@@ -144,9 +144,9 @@ function updatetestimonial(){
 
     var {name,position,text} = testimonialdatas[idx];
 
-    console.log(name);
-    console.log(position);
-    console.log(text);
+    // console.log(name);
+    // console.log(position);
+    // console.log(text);
     
     getcpmpanyname.textContent = name;
     getrole.textContent = position;
@@ -166,5 +166,62 @@ setInterval(updatetestimonial,10000);
     
 
 // End Testimonials
+// Start Properties Section
+
+filterby('all');
+
+function filterby(keyword){
+    // console.log(keyword);
+
+    var getfilters = document.getElementsByClassName('filters');
+
+    // console.log(getfilters);
+
+    if(keyword === 'all'){
+
+    }
+
+    for(var x = 0 ; x < getfilters.length;x++){
+        // console.log(getfilters[x]);
+
+        // console.log(getfilters[x].className.indexOf(keyword) > -1);
+
+        removeshowclass(getfilters[x],'show');
+
+        if(getfilters[x].className.indexOf(keyword) > -1){
+            addshowclass(getfilters[x],'show');
+        }
+        
+    }
+}
+
+function addshowclass(ele,opt){
+    // console.log(ele);
+    var getfilters = ele.className.split(" ");
+    // console.log(getfilters);
+
+    var getopst = opt.split(" ");
+    // console.log(getopt);
+    // console.log(getopt.length);
+    console.log(opt.length);
+
+    for(var y = 0 ; y < getopst.length ; y++){
+        // console.log(getopt[y]);
+
+        // console.log(getfilters.indexOf(getopt[y]) === -1 );
+        if(getfilters.indexOf (getopt[y]) === -1){
+            // ele.classList.addshowclass(getopt[y]);
+            
+            ele.className += " "+getopt[y];
+        }
+    }
+}
+
+function removeshowclass(ele,opt){
+    // console.log(ele);
+}
+// End Properties Section
 
 // 3TM
+
+// 1RM
